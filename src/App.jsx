c08c22f29683a34d227780766a68dc1a34dec34b
Jorbbey <jorbbey@gmail.com>
2025-03-24 14:18:32 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <p className='text-4xl  bg-blue-300'>Tailwind Test</p>
+    </>
+  )
+}
+
+export default App

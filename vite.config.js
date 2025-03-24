bae4c522794780a695afc168a6c2b346c48d339f
Jorbbey <jorbbey@gmail.com>
2025-03-24 15:05:03 +0100

    fix gh-pages bug

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss(),],
-  base: "/Brooklyn-Homes",
+  base: "/Brooklyn-Homes/",
 });

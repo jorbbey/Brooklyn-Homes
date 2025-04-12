e007930d0a732d9bb833e96f0724911fa891ac58
Jorbbey <jorbbey@gmail.com>
2025-04-12 12:22:46 +0100

    commit

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss()],
-  base: "./",
+  base: "/Brooklyn-Homes/",
   server: {
     historyApiFallback: true, // Fixes deep linking issues
   },

48f9598aac776d47c70c52768944fd5d4baaabdd
Jorbbey <jorbbey@gmail.com>
2025-05-03 02:59:50 +0100

    style blog

--- src/pages/SinglePost.jsx
+++ src/pages/SinglePost.jsx
@@ -67,7 +67,7 @@ function SinglePost() {
             By {post.author}
           </p>
         </div>
-        <h1 className="text-3xl font-bold mb-4">{post.title}</h1>
+        <h1 className="text-3xl font-bold mt-10 mb-4">{post.title}</h1>
         {post.image && (
           <img
             src={post.image}
@@ -77,12 +77,12 @@ function SinglePost() {
           />
         )}
         <div
-          className="prose prose-invert"
+          className="prose prose-invert my-5"
           dangerouslySetInnerHTML={{ __html: post.content }}
         />
 
         <div className="mt-10">
-          <h3 className="text-xl mb-2">💬 Leave a Reply</h3>
+          <h3 className="text-xl mb-2 mt-10">💬 Leave a Reply</h3>
           <textarea
             placeholder="Write your reply..."
             className="w-full p-2 border border-white rounded mb-2"
@@ -91,7 +91,7 @@ function SinglePost() {
           />
           <button
             onClick={handleReply}
-            className="bg-[#bc963f] text-white px-4 py-2 rounded hover:bg-blue-700"
+            className="bg-[#bc963f] text-white px-4 py-2 rounded my-5 cursor-pointer"
           >
             Submit Reply
           </button>

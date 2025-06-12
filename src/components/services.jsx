84d87c1c8d988e6bd68a9444efd43a3f309354f5
Jorbbey <jorbbey@gmail.com>
2025-06-12 04:36:49 +0100

    services update

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -2,10 +2,12 @@ import React, {useContext} from 'react'
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { FaBuilding } from "react-icons/fa";
-import { FaMoneyBillWave } from "react-icons/fa";
+import { FaHardHat } from "react-icons/fa";
+import { FaChartLine } from "react-icons/fa6";
 import { FaTools } from "react-icons/fa";
-import { FaHandshake } from "react-icons/fa";
+import { MdOutlineRealEstateAgent } from "react-icons/md";
+import { IoMdMegaphone } from "react-icons/io";
+import { FaTasks } from "react-icons/fa";
 import slide1 from '../assets/building-construction.jpg'
 import slide2 from '../assets/slide2-2.webp'
 import slide3 from '../assets/Brooklyn-Homes-Team/dou2.webp'
@@ -29,24 +31,34 @@ const Services = () => {
 
     const services = [
       {
-        icon: FaBuilding,
-        head: "Property Development",
-        desc: "Crafting modern, sustainable residential spaces.",
+        icon: FaHardHat,
+        head: "Real Estate Development",
+        desc: "We design, build, and deliver top-quality residential and commercial properties that blend aesthetics, functionality, and long-term value",
       },
       {
-        icon: FaMoneyBillWave,
-        head: "Real Estate Investment",
-        desc: "Unlocking high-return opportunities for investors.",
+        icon: FaChartLine,
+        head: "Real Estate Investment Advisory",
+        desc: "We provide sound investment guidance backed by local market insights, helping you grow wealth through real estate with clarity and confidence",
       },
       {
         icon: FaTools,
         head: "Facility Management",
-        desc: "Ensuring your property thrives with expert care.",
+        desc: "We maintain and manage residential and commercial buildings with a focus on safety, functionality, and tenant satisfaction",
       },
       {
-        icon: FaHandshake,
-        head: "Investment Advisory Services",
-        desc: "Guiding you to smart, future-proof decisions.",
+        icon: MdOutlineRealEstateAgent,
+        head: "Property Sales",
+        desc: "From land to completed homes, we connect buyers with the right real estate investment—honestly priced and professionally managed.",
+      },
+      {
+        icon: FaTasks,
+        head: "Project Management",
+        desc: "Our team handles everything from site supervision to budget control, ensuring your project is completed on time and to the highest standards.",
+      },
+      {
+        icon: IoMdMegaphone,
+        head: "Property Marketing & Branding",
+        desc: "We help you position and promote your property effectively using modern tools—photography, branding, and digital campaigns that convert.",
       },
     ];
   return (
@@ -57,54 +69,56 @@ const Services = () => {
           : "bg-white text-gray-950 my-10 md:my-30"
       }
     >
-      <h1 className="text-3xl lg:text-4xl my-20 mx-5">What We Offer</h1>
+      <h1 className="text-3xl lg:text-4xl my-10 lg:my-20 mx-5">
+        What We Offer
+      </h1>
       <div
         className={
           isDark
-            ? "flex flex-col-reverse lg:flex-row justify-between items-end bg-[url('/background1.webp')] bg-left-top bg-blend-multiply bg-gray-400/80"
-            : "flex flex-col-reverse lg:flex-row justify-between items-end bg-[url('/background1.webp')] bg-left-top bg-blend-multiply bg-gray-400/20"
+            ? "flex flex-col-reverse lg:flex-row justify-between items-center lg:items-center bg-[url('/background1.webp')] bg-left-top bg-blend-multiply bg-gray-400/80"
+            : "flex flex-col-reverse lg:flex-row justify-between items-center lg:items-center bg-[url('/background1.webp')] bg-left-top bg-blend-multiply bg-gray-400/20"
         }
       >
-        <main className="flex flex-wrap items-center justify-between lg:justify-start w-full md:w-[90%] lg:w-[70%]">
+        <main className="flex flex-col md:flex-row flex-wrap items-center justify-between lg:justify-start my-5 mx-2 md:my-0 w-[95%] lg:w-[70%]">
           {services.map((item) => (
             <div
               key={item.head}
-              className="text-center m-10 p-4 w-[90%] md:w-[35%] text-gray-950"
+              className="text-center m-auto my-5 p-4 w-[90%] md:w-[35%] text-gray-950"
             >
-              <item.icon className="text-4xl text-gray-600 w-full" />
-              <h1 className="text-xl my-5 w-full">{item.head}</h1>
+              <item.icon className="text-2xl md:text-3xl lg:text-4xl text-gray-600 w-full" />
+              <h1 className="text-lg md:text-xl my-2 md:my-5 w-full font-semibold">
+                {item.head}
+              </h1>
               <p className="my-3 text-sm">{item.desc}</p>
             </div>
           ))}
         </main>
 
-        <div className="w-full md:w-[90%] lg:w-[50%]">
-          <Slider {...settings}>
-            <div>
-              <img
-                src={slide1}
-                alt="Slide 1"
-                className="w-full h-[90vh] object-cover"
-                loading="lazy"
-              />
-            </div>
-            <div>
-              <img
-                src={slide3}
-                alt="Slide 3"
-                className="w-full h-[90vh] object-cover"
-                loading="lazy"
-              />
-            </div>
-            <div>
-              <img
-                src={slide4}
-                alt="Slide 2"
-                className="w-full h-[90vh] object-cover"
-                loading="lazy"
-              />
-            </div>
-          </Slider>
+        <div className="w-full md:w-[80%] lg:w-[50%]">
+          <div>
+            <img
+              src={slide4}
+              alt="Slide 1"
+              className="w-full h-[30vh] md:h-[50vh] object-cover my-2"
+              loading="lazy"
+            />
+          </div>
+          <div>
+            <img
+              src={slide1}
+              alt="Slide 3"
+              className="w-full h-[30vh] md:h-[50vh] object-cover"
+              loading="lazy"
+            />
+          </div>
+          <div>
+            <img
+              src={slide3}
+              alt="Slide 2"
+              className="w-full h-[30vh] md:h-[50vh] object-cover my-2"
+              loading="lazy"
+            />
+          </div>
         </div>
       </div>
     </section>
